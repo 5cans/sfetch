2c645e9a06bedc4e3c5982177cb18432a1de1ba6
Cans <49822575+5cans@users.noreply.github.com>
2019-07-31 15:50:54 -0400

    Create debian-sfetch.sh

---
+++ debian-sfetch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# pretty much the same as every other 'fetch' program but less than a KiB
+
+# __ DEFINING SYSTEM INFO __
+hostname=$(uname -n)
+kernel=$(uname -r)
+distro=$(lsb_release -ds)
+ip=$(wget http://ipecho.net/plain -O - -q ; echo)
+up=$(uptime -p | sed 's/up //')
+pack=$(dpkg -l | wc -l)
+
+# __ PRINTING __
+# using the ufetch logo because i like it
+echo
+echo '    _____     ' $hostname
+echo '   /  __ \\   ' $distro
+echo '  |  /    |   ' $kernel
+echo '  |  \\___-   ' $up
+echo '  \\          ' $pack
+echo '   \_         ' $ip
+echo       
+
+# I will make one of these for every distro I (regularly) use in the future
+
+
+
